b4ee38a2a5b319a9a5a94d9900b3b2d58e3b2ceb
yicr <yicr@users.noreply.github.com>
2026-01-09 08:01:25 +0000

    chore(deps): add devdeps pacakges

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,12 +1,33 @@
-import { typescript } from 'projen';
+import { typescript, javascript } from 'projen';
 const project = new typescript.TypeScriptProject({
+  authorName: 'yicr',
+  authorEmail: 'yicr@users.noreply.github.com',
   defaultReleaseBranch: 'main',
   name: 'jest-aws-cdk-asset-hash-serializer',
   projenrcTs: true,
-
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  description: 'this package is aws cdk construct asset hash serializer',
+  devDeps: [
+    'aws-cdk-lib',
+  ],
+  jestOptions: {
+    jestConfig: {
+      snapshotSerializers: ['<rootDir>/src/index.ts'],
+    },
+  },
+  publishTasks: true,
+  releaseToNpm: true,
+  npmAccess: javascript.NpmAccess.PUBLIC,
+  minNodeVersion: '16.0.0',
+  workflowNodeVersion: '22.4.x',
+  // depsUpgradeOptions: {
+  //   workflowOptions: {
+  //     labels: ['auto-approve', 'auto-merge'],
+  //     schedule: javascript.UpgradeDependenciesSchedule.expressions(['0 16 * * 4']), // every thursday 16:00 (JST/FRI/02:00)
+  //   },
+  // },
+  autoApproveOptions: {
+    secret: 'GITHUB_TOKEN',
+    allowedUsernames: ['yicr'],
+  },
 });
 project.synth();
